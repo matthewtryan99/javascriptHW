59c667aa13212d5b82e620e0c7096f601daf1e72
Matt Ryan <matthewtryan99@gmail.com>
2020-02-06 15:24:23 -0600

    assignment 3 city temperatures

--- assignment3.js
+++ assignment3.js
@@ -1,4 +1,4 @@
-let ary = [-1,5,6,2,-8,10,12,16];
+// let ary = [-1,5,6,2,-8,10,12,16];
 // let posAry = ary.filter(function(element){
 //     return element > 0;
 // })
@@ -11,8 +11,21 @@ let ary = [-1,5,6,2,-8,10,12,16];
 
 // console.log(evenAry);
 
-let squareAry = ary.map(function(element){
-    return element ** 2;
-})
+// let squareAry = ary.map(function(element){
+//     return element ** 2;
+// })
 
-console.log(squareAry);
+// console.log(squareAry);
+
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 } 
+    ];
+
+    var newCities = cities.filter(function(element){
+        return element.temperature < 70.0;
+    })
+
+    console.log(newCities);
