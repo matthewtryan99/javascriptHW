0a1a60715060806f6e79587eef1218b1497b674e
Matt Ryan <matthewtryan99@gmail.com>
2020-02-06 15:20:06 -0600

    assignment 2 positive numbers

--- assignment3.js
+++ assignment3.js
@@ -1,6 +1,12 @@
-let ary = [1,5,6,2,8,10,12,16];
+let ary = [-1,5,6,2,-8,10,12,16];
 let posAry = ary.filter(function(element){
-    return (element % 2) == 0;
+    return element > 0;
 })
 
 console.log(posAry);
+
+// let evenAry = ary.filter(function(element){
+//     return (element % 2) == 0;
+// })
+
+// console.log(evenAry);
